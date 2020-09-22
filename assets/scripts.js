8012dde5246642d9d0bd4b91f1a58de7c506e21e
juliapyke <pyke.julia@gmail.com>
2020-09-22 14:59:06 -0700

    add scroll to top button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,14 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+//scroll to top button
+const button = document.getElementsByClassName('scroll-to-top');
+if ("scrollBehavior" in document.documentElement.style) {
+  button[0].addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    return false;
+  }, false);
+} else {
+  button.style.visibility = 'hidden';
+}
