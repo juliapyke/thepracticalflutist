eb16db38197ac79eb1f120e8a5b3c8e8e9d4a67c
juliapyke <pyke.julia@gmail.com>
2020-09-23 15:07:18 -0700

    scroll to top only appears when scrolling up

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,9 +3,21 @@ $(function () {
 });
 
 //scroll to top button
-const button = document.getElementsByClassName('scroll-to-top');
+const button = document.getElementsByClassName('scroll-to-top')[0];
+var lastScrollTop = 0;
 if ("scrollBehavior" in document.documentElement.style) {
-  button[0].addEventListener('click', () => {
+  window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document
+      .documentElement.scrollTop;
+        if (scrollTop > lastScrollTop){
+          button.style.bottom="-60px";
+        } else {
+          button.style.bottom="0";
+        }
+        lastScrollTop = scrollTop;
+
+  })
+  button.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     return false;
   }, false);
