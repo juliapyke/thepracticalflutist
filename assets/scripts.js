9c21d65ed07db15005ef4f9522a0dda5e6628da5
juliapyke <pyke.julia@gmail.com>
2020-09-23 18:50:11 -0700

    improved scroll to top button

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,16 +5,24 @@ $(function () {
 //scroll to top button
 const button = document.getElementsByClassName('scroll-to-top')[0];
 var lastScrollTop = 0;
+var scrollBottom = 0;
 if ("scrollBehavior" in document.documentElement.style) {
   window.addEventListener("scroll", function(){
     var scrollTop = window.pageYOffset || document
       .documentElement.scrollTop;
-        if (scrollTop > lastScrollTop || scrollTop === 0){
+      var scrollUp = scrollTop < lastScrollTop;
+        if (scrollTop === 0) {
           button.style.bottom="-60px";
           lastScrollTop = scrollTop;
-        } else if ((lastScrollTop - scrollTop) > 1000){
-          button.style.bottom="0";
+        } else if (scrollUp) {
+          if ((scrollBottom - scrollTop) > 1000) {
+            button.style.bottom="0";      
+          }
+        } else {
+          button.style.bottom="-60px";
+          scrollBottom = scrollTop;
         }
+        lastScrollTop = scrollTop;
   })
   button.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
